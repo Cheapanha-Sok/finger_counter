65c138abd24ad80ab8d905c6e94006d42d10d62d
sokcheapanha <sokcheapanha11@gmail.com>
2024-11-09 22:14:56 +0700

    fixed count on the left hand

--- FingerCounting.py
+++ FingerCounting.py
@@ -1,6 +1,5 @@
 import cv2
 import time
-import os
 import HandTrackingModule as htm
 
 # Camera setup for width and height
@@ -9,75 +8,50 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
-# use os to store the folder FingerImages
-folderPath = "FingerImages"
-myList = os.listdir(folderPath)
-# print(myList)
-pTime = 0
-overlayList = []
-
-for imPath in myList:
-    image = cv2.imread(f'{folderPath}/{imPath}')  # Load each image
-    resized_image = cv2.resize(image, (200, 200))  # Resize to 200x200
-    overlayList.append(resized_image)
-    
-# print(len(overlayList))
-#Initialize hand detector class
-detector = htm.HandDetector(detectionCon=0.75 , maxHands=1)
+detector = htm.HandDetector(detectionCon=0.75, maxHands=1)
 tipIds = [4, 8, 12, 16, 20]
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     
-    #create a list of the landmark that we detect
-    lmList = detector.findPosition(img, draw=False)
+    # Create a list of the landmarks detected
+    lmList = detector.findPosition(img)
     
     if len(lmList) != 0:
         fingers = []
         
-        #Thumb
-        if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
-            fingers.append(1)
+        # Determine if the hand is left or right
+        if lmList[tipIds[1]][1] < lmList[0][1]:
+            # Left hand
+            if lmList[tipIds[0]][1] < lmList[tipIds[0]-1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
         else:
-            fingers.append(0)
-            
-        # 4 Fingers
+            # Right hand
+            if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        
+        # 4 Fingers (index to pinky)
         for id in range(1, 5):
             if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
                 
-        # print(fingers)
         totalFingers = fingers.count(1)
-        # print(totalFingers)
-    
-        h,w,c = overlayList[totalFingers-1].shape
-        img[0:h, 0:w] = overlayList[totalFingers-1]
         
-        cv2.rectangle(img, (20, 225), (170,425), 
-                      (0,255,0), cv2.FILLED)
-        cv2.putText(img, str(totalFingers), (45,375), cv2.FONT_HERSHEY_PLAIN,
-                    10, (255,0,0), 25)
-    
-    #Calculate the frame rate
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
-    
-    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
-                3, (255,0,0),3)
+        # Draw the rectangle on the top left corner
+        cv2.rectangle(img, (20, 20), (170, 220), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 170), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
 
-    # if not success:
-    #     print('Error reading frame')
-    #     break
-
-    # frame = detector.findHands(frame)
-
-    # Show the processed image
     cv2.imshow('finger counter', img)
-
-    # Break the loop on 'q' key press
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-import time
 
 class HandDetector:
     """
@@ -45,7 +44,8 @@ class HandDetector:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
 
         return img
-    def findPosition(self, img, handNo=0, draw=True):
+    
+    def findPosition(self, img, handNo=0):
         lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
@@ -55,26 +55,4 @@ class HandDetector:
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 # print(id, cx, cy)
                 lmList.append([id, cx, cy])
-                if draw:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
         return lmList
-def main():
-    pTime = 0
-    cTime = 0
-    cap = cv2.VideoCapture(1)
-    detector = HandDetector()
-    while True:
-        success, img = cap.read()
-        img = detector.findHands(img)
-        lmList = detector.findPosition(img)
-        if len(lmList) != 0:
-            print(lmList[4])
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                    (255, 0, 255), 3)
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
-if __name__ == "__main__":
-    main()
